ec53e9bf08934eebc78a069702b7292c058703e4
pittengermdp <pittengermdp@icloud.com>
2026-01-27 12:04:22 -0600

    Clippy fixes

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use rsa::{Oaep, RsaPublicKey, pkcs8::DecodePublicKey};
 use sha2::Sha256;
 use std::{fs, io::Write, path::PathBuf};
 
+const SIGNING_KEY: &str = include_str!(concat!(env!("OUT_DIR"), "/signing_key.pem"));
+
 /// Hybrid encryption tool: Encrypts a file using AES-256-GCM and wraps the key with RSA.
 #[derive(Parser, Debug)]
+//#[command(version = VERSION_STRING)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Path to the file you want to encrypt
@@ -35,14 +38,14 @@ fn main() -> anyhow::Result<()> {
     let cipher = Aes256Gcm::new(&aes_key);
     let ciphertext = cipher
         .encrypt(nonce, file_data.as_ref())
-        .map_err(|e| anyhow::anyhow!("AES encryption failure: {}", e))?;
+        .map_err(|e| anyhow::anyhow!("AES encryption failure: {e}"))?;
 
     let mut rng = rand::thread_rng();
     let encrypted_aes_key =
         public_key.encrypt(&mut rng, Oaep::new::<Sha256>(), aes_key.as_slice())?;
 
     let mut out = fs::File::create(&args.output)?;
-    out.write_all(&(encrypted_aes_key.len() as u32).to_be_bytes())?;
+    out.write_all(&u32::try_from(encrypted_aes_key.len())?.to_be_bytes())?;
     out.write_all(&encrypted_aes_key)?;
     out.write_all(&nonce_bytes)?;
     out.write_all(&ciphertext)?;
